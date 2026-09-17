8516971376da92161fd40ad26f48e1414444d723
rudymuntasser-beep <rudy.muntasser@gmail.com>
2026-09-17 20:23:11 +0200

    feat: add STARSHIP service worker

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='starship-v4-shell-1';
+const SHELL=['./','./index.html','./app.js','./ui-core.js','./explanations.js','./officers.js','./command-core.js','./manifest.webmanifest','./icons/starship-192.webp','./icons/starship-512.webp'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return r}).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))))});
